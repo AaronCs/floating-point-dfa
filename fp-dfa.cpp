90227fbe9a82a1cd444f6587712e9c5140cd2463
Aaron <aachansy2007@gmail.com>
2018-05-26 18:38:22 -0700

    Fix functions
    I think this works better.

--- fp-dfa.cpp
+++ fp-dfa.cpp
@@ -10,101 +10,123 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
 
 using namespace std;
 
-void print_result(bool result);
-void reset_state(int curr_state);
-void state_transition(int curr_state, bool result);
-void state_1(char c, int curr_state, bool result);
-void state_2(char c, int curr_state, bool result);
-void state_3(char c, int curr_state, bool result);
-void state_4(char c, int curr_state, bool result);
-void state_5(char c, int curr_state, bool result);
-void state_6(char c, int curr_state, bool result);
-void state_7(char c, int curr_state, bool result);
-void state_8(char c, int curr_state, bool result);
+void print_result(int curr_state, bool & result);
+void reset_state(int & curr_state);
+void state_transition(char c, int & curr_state);
+int state_1(char c);
+int state_2(char c);
+int state_3(char c);
+int state_4(char c);
+int state_5(char c);
+int state_6(char c);
+int state_7(char c);
+int state_8(char c);
 
 int main() {
+    string test = ".";
     int curr_state = 1; // Initial State is 1.
     bool result = false;
     
-    print_result(result);
+    state_transition(test[0], curr_state);
+    cout << "CURRENT STATE: " << curr_state << endl;
+    print_result(curr_state, result);
 
     reset_state(curr_state);
     return 0;
 }
 
-void reset_state(int curr_state) {
+void reset_state(int & curr_state) {
     // Not sure if good idea, 
     // but I feel like it's good for readability.
     curr_state = 1;
 }
 
-void print_result(bool result) {
+void print_result(int curr_state, bool & result) {
+    switch(curr_state) {
+        case 2:
+        case 4:
+        case 5:
+            result = true;
+            break;
+        default:
+            result = false;
+    }
     if(result) cout << "Accept" << endl;
     else {
         cout << "Reject" << endl;
     }
 }
 
-void state_transition(int curr_state, bool result) {
-    char c = 'a';
+void state_transition(char c, int & curr_state) {
+    // FIXME: Not sure if result should be decided in this function or another
     switch(curr_state) {
         case 1:
-            state_1(c, curr_state, result);
+            curr_state = state_1(c);
             break;
         case 2:
-            state_2(c, curr_state, result);
+            curr_state = state_2(c);
             break;
         case 3:
-            state_3(c, curr_state, result);
+            curr_state = state_3(c);
             break;
         case 4:
-            state_4(c, curr_state, result);
+            curr_state = state_4(c);
             break;
         case 5:
-            state_5(c, curr_state, result);
+            curr_state = state_5(c);
             break;
         case 6:
-            state_6(c, curr_state, result);
+            curr_state = state_6(c);
             break;
         case 7:
-            state_7(c, curr_state, result);
+            curr_state = state_7(c);
             break;
         case 8:
-            state_8(c, curr_state, result);
+            curr_state = state_8(c);
+            break;
+        default:
+            // Dead State
+            curr_state = 0;
             break;
     }
 }
 
-void state_1(char c, int curr_state, bool result) {
-
+int state_1(char c) {
+    string nums = "0123456789";
+    if(nums.find(c) != string::npos) {
+        return 2;
+    }
+    if(c == '.') return 8;
+    return 0;
 }
-void state_2(char c, int curr_state, bool result) {
-
-}
-
-void state_3(char c, int curr_state, bool result) {
-
-}
-
-void state_4(char c, int curr_state, bool result) {
+int state_2(char c) {
 
 }
 
-void state_5(char c, int curr_state, bool result) {
+int state_3(char c) {
 
 }
 
-void state_6(char c, int curr_state, bool result) { 
-}
-
-void state_7(char c, int curr_state, bool result) {
+int state_4(char c) {
 
 }
 
-void state_8(char c, int curr_state, bool result) {
+int state_5(char c) {
+
+}
+
+int state_6(char c) { 
+}
+
+int state_7(char c) {
+
+}
+
+int state_8(char c) {
 
 }
 
